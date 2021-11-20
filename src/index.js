5628fc8eba902559b6487d2c76dda5ad2c13d302
Luciana Sugeta <lulusugeta@gmail.com>
2021-11-19 19:01:18 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import manageAuthorsAndBooks from './reducers/manageAuthorsAndBooks';
+// import manageAuthorsAndBooks from './reducers/manageAuthorsAndBooks';
+// new file name:
+import rootReducer from './reducers/manageAuthorsAndBooks';
 
-
-const store = createStore(manageAuthorsAndBooks, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// const store = createStore(manageAuthorsAndBooks, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// rootReducer is the default export of manageAuthorsAndBooks.js, the app will continu to work with this code above, but we can update it to reflect the new name
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,3 +1,73 @@
+// Redux's combineReducers() function will allow us to write separate reducers for each of the resources in our application (autorhos and books)
+// Each reducer is passed to the combineReducers() function, which will produce a single reducer
+// We then pass that combined reducer to the store in src/index.js
+import { combineReducers } from "redux";
+
+// this package handle unique ID generation
+import uuid from "uuid";
+
+// We're telling Redux to produce a reducer which will return a state that has 2 keys:
+// Key 1: Authors with a value equal to the return value of the authorsReducer()
+// Key 2: Books with a value equal to the return value of booksReducer()
+// Both keys return a default state of an empty array -> []
+// This will produce the same initial state that we originally specified (see code below state = {authors: [], books: []})
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+})
+
+export default rootReducer;
+
+function booksReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_BOOK":
+      return [...state, action.book]
+
+    case "REMOVE_BOOK":
+      idx = state.findIndex(book => book.id === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)]
+
+    default:
+      return state
+  }
+}
+
+// This reducer only concerns itself with its own slice of the state
+// The array that the authorsReducer() returns will be the value associated with the key of authors in the state object
+// Consequently, the authorsReducer() should only receive as its state argument the value of state.authors (the authors array)
+// We no longer need to retrieve the list of authors with a call to state.authors, simply call state
+function authorsReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_AUTHOR":
+      return [...state, action.author]
+
+    case "REMOVE_AUTHOR":
+      idx = state.findIndex(author => author.id === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)]
+
+    // Check to see if any of the authorName's currently stored in state match the name dispatched from the Book input component
+    // If the name already exists, state is returned unchanged
+    // If the name is not present, it is added to the authors array
+    // This way, we're able to add authors and books
+    case "ADD_BOOK":
+      let existingAuthor = state.filter(
+        author => author.authorName === action.book.authorName
+      )
+      if (existingAuthor.length > 0) {
+        return state
+      } else {
+        return [...state, { authorName: action.book.authorName, id: uuid() }]
+      }
+
+    default:
+      return state
+  }
+}
+
+// Before combineReducers() function:
+/*
 export default function bookApp(
   state = {
     authors: [],
@@ -13,7 +83,7 @@ export default function bookApp(
         authors: [...state.authors],
         books: [...state.books, action.book]
       };
- 
+
     case "REMOVE_BOOK":
       idx = state.books.findIndex(book => book.id === action.id);
       return {
@@ -21,14 +91,14 @@ export default function bookApp(
         authors: [...state.authors],
         books: [...state.books.slice(0, idx), ...state.books.slice(idx + 1)]
       };
- 
+
     case "ADD_AUTHOR":
       return {
         ...state,
         books: [...state.books],
         authors: [...state.authors, action.author]
       };
- 
+
     case "REMOVE_AUTHOR":
       idx = state.authors.findIndex(author => author.id === action.id);
       return {
@@ -36,8 +106,9 @@ export default function bookApp(
         books: [...state.books],
         authors: [...state.authors.slice(0, idx), ...state.authors.slice(idx + 1)]
       };
- 
+
     default:
       return state;
   }
 }
+*/
